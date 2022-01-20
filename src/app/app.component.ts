14b86cd712b89a4f6eac18eb9ffe00476d971915
syedahmadsaeed3 <ahmad@iiinigence.com>
2022-01-20 16:58:17 +0500

    simple context menu and responsive table height

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
+
+
 import { Component, OnInit } from '@angular/core';
-import { VirtualScrollService } from '@syncfusion/ej2-angular-treegrid';
+import { VirtualScrollService, ToolbarItems } from '@syncfusion/ej2-angular-treegrid';
 import { dataSource, virtualData } from './datasource';
+import { ContextMenuService, EditService, SortService, ResizeService } from '@syncfusion/ej2-angular-grids';
+import { ContextMenuItem, EditSettingsModel } from '@syncfusion/ej2-angular-grids';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
   templateUrl: "./app.component.html",
-  providers: [VirtualScrollService]
+  providers: [VirtualScrollService, ContextMenuService, EditService, SortService, ResizeService]
 })
 export class AppComponent implements OnInit {
   public data: Object[] = [{}];
+  public editSettings: EditSettingsModel = {};
+  public contextMenuItems: ContextMenuItem[] = [];
 
   ngOnInit(): void {
     dataSource();
+
     this.data = virtualData;
+    this.editSettings = {
+      allowEditing: true, allowAdding: true, allowDeleting: true, mode: "Dialog", showConfirmDialog: true,
+      showDeleteConfirmDialog: true
+    };
+    this.contextMenuItems = ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending', 'Edit', 'Delete', 'Save', 'Cancel'];
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,11 @@
 import { NgModule,ViewChild } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TreeGridModule } from '@syncfusion/ej2-angular-treegrid';
+import { PageService, SortService, FilterService,EditService,ToolbarService } from '@syncfusion/ej2-angular-treegrid';
+import { ResizeService, ExcelExportService, PdfExportService, ContextMenuService } from '@syncfusion/ej2-angular-treegrid';
 import { AppComponent } from './app.component';
+import {ButtonModule} from '@syncfusion/ej2-angular-buttons';
+import { DropDownListAllModule } from '@syncfusion/ej2-angular-dropdowns';
 
 /**
  * Module
@@ -10,9 +14,19 @@ import { AppComponent } from './app.component';
 @NgModule({
     imports: [
         BrowserModule,
-        TreeGridModule
+        TreeGridModule,
+        ButtonModule,
+        DropDownListAllModule,
     ],
     declarations: [AppComponent],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    providers: [PageService,
+                SortService,
+                FilterService,
+                EditService,
+                ResizeService, 
+                ExcelExportService, 
+                PdfExportService, ContextMenuService,
+                ToolbarService]
 })
 export class AppModule { }
